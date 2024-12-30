1392607ce60a5757475e7d874335272aa9ae7ef3
shree007 <shreeprakashagrahari05@gmail.com>
2024-12-30 17:14:35 +0530

    dec-30 add terraform modules for vpc

---
+++ infrastructure/terraform-modules/vpc/output.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "vpc_ipv6_cidr_block" {
+  description = "The IPv6 CIDR block of the VPC"
+  value       = aws_vpc.main.ipv6_cidr_block
+}
